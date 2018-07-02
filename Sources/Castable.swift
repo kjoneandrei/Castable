0fd3f4901c0fb317a70ea5705b1107da87ff0cf7
anho <anho@nodes.dk>
2018-07-02 20:40:27 +0200

    First commit

---
+++ Sources/Castable.swift
@@ -0,0 +1,9 @@
+//
+//  Castable.swift
+//  Castable
+//
+//  Created by Andrei Hogea on 02/07/2018.
+//  Copyright © 2018 Castable. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/CastableTests/CastableTests.swift
@@ -0,0 +1,23 @@
+//
+//  CastableTests.swift
+//  Castable
+//
+//  Created by Andrei Hogea on 02/07/2018.
+//  Copyright © 2018 Castable. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Castable
+
+class CastableTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(Castable().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
